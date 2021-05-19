1425014872dbad93bb6aef95b6ded6b80bdde3c0
YUNHO KIM <F35jsflove@gmail.com>
2021-05-19 22:52:49 +0900

    policy iteration

---
+++ Lecture3/policy_iteration.py
@@ -0,0 +1,55 @@
+import copy
+
+def greedy(valuefunction, pos): #returns the "best" move, refer to the given value funciton
+    bestoption = [0, 0]
+    bestvalue = -10000000
+    options = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+    values = []
+    for option in options:
+        if pos[0]+option[0]>=0 and pos[0]+option[0]<4 and pos[1]+option[1]>=0 and pos[1]+option[1]<4:
+            value = valuefunction[pos[0]+option[0]][pos[1]+option[1]]
+            values.append(value)
+        else:
+            values.append(None)
+        
+    ret = []
+    maxval = -1000
+    for value in values:
+        if value is not None:
+            if value>maxval:
+                maxval = value
+    for i in range(4):
+        if values[i] == maxval:
+            ret.append(options[i])
+    return ret
+
+valuefunction = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]] # the value function("how good the state is")
+newvaluefunction = copy.deepcopy(valuefunction)
+gamma = 1 # the discount factor 0: short sighted, 1: long sighted
+
+for i in range(10):
+    print(i)
+    valuefunction = copy.deepcopy(newvaluefunction)
+    for row in range(4):
+        for col in range(4):
+            if row==0 and col==0:
+                continue
+            if row == 3 and col == 3:
+                continue
+            result = 0
+
+            moves = greedy(valuefunction, [row, col]) # greedy policy is updated every iteration
+
+            for move in moves:
+                result += valuefunction[row+move[0]][col+move[1]]
+
+            newvaluefunction[row][col] = gamma*result/len(moves)-1
+    
+    for row in newvaluefunction:
+        for v in row:
+            print(v, end = ' ')
+        print()
+
+
+    
+    
