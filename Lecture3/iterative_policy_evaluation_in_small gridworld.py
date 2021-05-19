60c669dca8e2a701b1fd1a383a7d0ae8b0fc2313
YUNHO KIM <F35jsflove@gmail.com>
2021-05-19 22:32:10 +0900

    added comment

--- Lecture3/iterative_policy_evaluation_in_small gridworld.py
+++ Lecture3/iterative_policy_evaluation_in_small gridworld.py
@@ -1,8 +1,9 @@
+#rules: policy: random move, the game terminates when it reaches to 0, 0 or 3, 3. Each moves gives -1 reward
 import copy
 
-valuefunction = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+valuefunction = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]] # the value function("how good the state is")
 newvaluefunction = copy.deepcopy(valuefunction)
-
+gamma = 0.5 # the discount factor 0: short sighted, 1: long sighted
 
 for i in range(1000):
     print(i)
@@ -19,7 +20,7 @@ for i in range(1000):
             result += valuefunction[row][max(col-1, 0)]
             result += valuefunction[row][min(col+1, 3)]
 
-            newvaluefunction[row][col] = result/4-1
+            newvaluefunction[row][col] = gamma*result/4-1
     
     for row in newvaluefunction:
         for v in row:
